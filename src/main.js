43b70c632c4634d85749c887c7280c9d75c56da4
Vladislav Popov <vlad_np@ukr.net>
2025-11-21 06:31:39 +0100

    fix bug with paths

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import './js/quote-of-the-day.js';
 
-import './favorites-btn.js';
-import './favorites-page.js';
+import './js/favorites-btn.js';
+import './js/favorites-page.js';
 
 import './js/categories.js';
 
