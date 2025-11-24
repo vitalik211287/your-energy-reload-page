c7230f33f59cf9bfe814f28cc7e0bb2d2136a6ba
Vladislav Popov <vlad_np@ukr.net>
2025-11-24 17:29:32 +0100

    add loader settimeout

--- src/js/loader.js
+++ src/js/loader.js
@@ -7,6 +7,8 @@ export function startLoader() {
   if (ref.loaderBg) ref.loaderBg.classList.add('bg-loader');
 }
 export function cancelLoader() {
-  if (ref.loaderOverlay) ref.loaderOverlay.classList.add('hidden');
-  if (ref.loaderBg) ref.loaderBg.classList.remove('bg-loader');
+  setTimeout(() => {
+    if (ref.loaderOverlay) ref.loaderOverlay.classList.add('hidden');
+    if (ref.loaderBg) ref.loaderBg.classList.remove('bg-loader');
+  }, 250);
 }
