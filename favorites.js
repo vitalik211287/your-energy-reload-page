f5d1311fde5f5698cad72d5fd591c2e1e056c3f9
StudentVlad5 <vlad_np@ukr.net>
2025-11-25 09:41:36 +0000

    Deploying to gh-pages from @ StudentVlad5/your-energy@3e2e67d29a3906c2b088ab4c1decc8eae91d1ac9 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import{c as e,f as t,g as n,h as o,j as i,M as a,k as s,m as d}from"./assets/modal-rating-content-DiH8-Yun.js";import"./assets/vendor-3BNOZKAJ.js";document.addEventListener("DOMContentLoaded",()=>{setTimeout(()=>e(),300),M()});function M(){t(a.RATING,i,o),t(a.EXERCISE,d,s),n()}
+import{c as a,e as t,f as n,g as o,h as i,M as e,j as s,k as d}from"./assets/modal-rating-content-B3okQ2MX.js";import"./assets/vendor-3BNOZKAJ.js";document.addEventListener("DOMContentLoaded",()=>{setTimeout(()=>a(),300),M()});function M(){t(e.RATING,i,o),t(e.EXERCISE,d,s),n()}
 //# sourceMappingURL=favorites.js.map
